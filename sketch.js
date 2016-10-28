3c2fe0bc754e38e102c5a8f975dc32602cffbecf
marthematicist <hite.32@gmail.com>
2016-10-28 08:58:31 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,8 +2,7 @@
 function zEquals( x , y ) {
   //x = x - 1.5;
   //y = y - 1.5;
-  //var a = -0.2*(1-0.7/6*sqrt((x*x + y*y) ))*cos( 9 *sqrt( (x*x + y*y) ) ) ;
-  var a = -0.1*(x*x + y*y);
+  var a = -0.2*(1-0.7/6*sqrt((x*x + y*y) ))*cos( 9 *sqrt( (x*x + y*y) ) ) ;
   return a;
 }
 
