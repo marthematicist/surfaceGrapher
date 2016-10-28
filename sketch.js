31999517b78e7e3f95579456aeef5bd1400673e2
marthematicist <hite.32@gmail.com>
2016-10-27 23:06:24 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,100 @@
 // FUNCTION: zEquals
-var zEquals = function( x , y ) {
-  return x*x + y*y;
+function zEquals( x , y ) {
+  x = x - 0;
+  y = y - 0;
+  var a = -0.2*(1-0.7/3*sqrt((x*x + y*y) ))*cos( 9 *sqrt( (x*x + y*y) ) ) ;
+  x = x + 1;
+  y = y + 1;
+  var b = -0.1*(1-0.5/3*sqrt((x*x + y*y) ))*cos( 18*sqrt( (x*x + y*y) ) ) ;
+  x = x + 1;
+  y = y + 2;
+  var c = -0.1*(1-0.5/3*sqrt((x*x + y*y) ))*cos( 18*sqrt( (x*x + y*y) ) ) ;
+  return a;
+}
+
+
+
+setupGlobalVariables = function() {
+  // pixel resolution for screen
+  xRes = windowWidth;
+  yRes = windowHeight;
+  minRes = min( xRes , yRes );
+  maxRes = max( xRes , yRes );
+  
+  // region dimensions
+  xMin = -3 ;
+  yMin = -3 ;
+  xMax = 3 ;
+  yMax = 3 ;
+  xDim = xMax - xMin;
+  yDim = yMax - yMin;
+  minDim = min( xDim , yDim );
+  maxDim = max( xDim , yDim );
+  
+  // number of cell rows and columns
+  M = 100;
+  N = 100;
+  
+  // screen factor ( regionPosition*sFactor = screenPosition )
+  sFactor = minRes / maxDim * 0.8;
+  
+  // initialize Region
+  R = new Region( xMin , yMin , xMax , yMax , M , N );
+}
+
+function setup() {
+  // set up global variables
+  setupGlobalVariables();
+  
+  // set canvas dimensions and 3D mode
+  createCanvas( xRes , yRes , WEBGL );
+  console.log( 'all set u');
+  
+
+}
+
+function draw() {
+  rotateX( -PI*0.5 );
+  rotateX( mouseY*0.001);
+  rotateZ( mouseX*0.001);
+  
+  drawMesh( R );
+
+}
+
+// CLASS DEFINITION: Cell
+class Cell {
+  constructor( x , y , w , h ) {
+    // position vectors for p0-04
+    this.p0 = createVector( x , y , 0 );
+    this.p1 = createVector( x+w , y , 0 );
+    this.p2 = createVector( x+w , y+h , 0 );
+    this.p3 = createVector( x , y+h , 0 );
+    this.p4 = createVector( x+0.5*w , y+0.5*h , 0 );
+    
+    // initialize min and max z values for Cell
+    this.zMin = 0;
+    this.zMax = 0;
+    
+    // set z values for all points
+    this.p0.z = zEquals( this.p0.x , this.p0.y );
+    this.p1.z = zEquals( this.p1.x , this.p1.y );
+    this.p2.z = zEquals( this.p2.x , this.p2.y );
+    this.p3.z = zEquals( this.p3.x , this.p3.y );
+    this.p4.z = zEquals( this.p4.x , this.p4.y );
+    
+    // find min and max z values for Cell
+    if( this.p0.z < this.zMin ) { this.zMin = this.p0.z; }
+    if( this.p0.z > this.zMax ) { this.zMax = this.p0.z; }
+    if( this.p1.z < this.zMin ) { this.zMin = this.p1.z; }
+    if( this.p1.z > this.zMax ) { this.zMax = this.p1.z; }
+    if( this.p2.z < this.zMin ) { this.zMin = this.p2.z; }
+    if( this.p2.z > this.zMax ) { this.zMax = this.p2.z; }
+    if( this.p3.z < this.zMin ) { this.zMin = this.p3.z; }
+    if( this.p3.z > this.zMax ) { this.zMax = this.p3.z; }
+    if( this.p4.z < this.zMin ) { this.zMin = this.p4.z; }
+    if( this.p4.z > this.zMax ) { this.zMax = this.p4.z; }
+  }
 }
 
 // CLASS DEFINITION: Region
@@ -12,11 +106,13 @@ class Region {
     this.xMax = xMax;
     this.yMax = yMax;
     
-    // dimensions of region
+    // dimensions of Region
     this.xDim = xMax - xMin;
     this.yDim = yMax - yMin;
+    this.minDim = min( xDim , yDim );
+    this.maxDim = max( xDim , yDim );
     
-    // center of region
+    // center of Region
     this.center = createVector( 0.5*(xMin+xMax) , 0.5*(yMin+yMax) , 0 );
     
     // number of columns (M) and rows (N) (of cells)
@@ -26,6 +122,10 @@ class Region {
     // dimensions of cells
     this.cellWidth = this.xDim / this.M;
     this.cellHeight = this.yDim / this.N;
+    
+    // initialize min and max z values for Region
+    this.zMin = 0;
+    this.zMax = 0;
     
     // initialize 2-dimentional array of cell objects
     // this.cell[m][n] is the cell in the mth column and nth row
@@ -37,75 +137,83 @@ class Region {
     // set values for each cell in the array
     for( var m = 0 ; m < this.M ; m++ ) {
       for( var n = 0 ; n < this.N ; n++ ) {
-        this.cell[m][n] = new Cell( m*this.cellWidth , n*this.cellHeight , this.cellWidth , this.cellHeight );
+        this.cells[m][n] = new Cell( this.xMin + m*this.cellWidth ,
+                                     this.yMin + n*this.cellHeight ,
+                                     this.cellWidth ,
+                                     this.cellHeight );
+      }
+    }
+    
+    // find the min and max z values for the Region
+    for( var m = 0 ; m < this.M ; m++ ) {
+      for( var n = 0 ; n < this.N ; n++ ) {
+        if( this.cells[m][n].zMin < this.zMin ) { this.zMin = this.cells[m][n].zMin ; }
+        if( this.cells[m][n].zMax > this.zMax ) { this.zMax = this.cells[m][n].zMax ; }
       }
     }
     
   }
-}
-
-
-
-// CLASS DEFINITION: Cell
-class Cell {
-  constructor( x , y , w , h ) {
-    this.p0 = createVector( x , y , 0 );
-    this.p1 = createVector( x+w , y , 0 );
-    this.p2 = createVector( x+w , y+h , 0 );
-    this.p3 = createVector( x , y+h , 0 );
-    this.p4 = createVector( x+0.5*w , y+0.5*h , 0 );
-    this.zMin = 0;
-    this.zMax = 0;
-  }
-}
-
-// CLASS METHODS: Cell
-Cell.prototype.calculateZ = function() {
-  this.p0.z = zEquals( this.p0.x , this.p0.y );
-  if( this.p0.z < this.zMin ) {
-    this.zMin = this.p0.z;
-  }
-  if( this.p0.z > this.zMax ) {
-    this.zMax = this.p0.z;
-  }
-
-  this.p1.z = zEquals( this.p1.x , this.p1.y );
-  if( this.p1.z < this.zMin ) {
-    this.zMin = this.p1.z;
-  }
-  if( this.p1.z > this.zMax ) {
-    this.zMax = this.p1.z;
-  }
   
-  this.p2.z = zEquals( this.p2.x , this.p2.y );
-  if( this.p2.z < this.zMin ) {
-    this.zMin = this.p2.z;
-  }
-  if( this.p2.z > this.zMax ) {
-    this.zMax = this.p2.z;
-  }
-  
-  this.p3.z = zEquals( this.p3.x , this.p3.y );
-  if( this.p3.z < this.zMin ) {
-    this.zMin = this.p3.z;
-  }
-  if( this.p3.z > this.zMax ) {
-    this.zMax = this.p3.z;
-  }
-  
-  this.p4.z = zEquals( this.p4.x , this.p4.y );
-  if( this.p4.z < this.zMin ) {
-    this.zMin = this.p4.z;
-  }
-  if( this.p4.z > this.zMax ) {
-    this.zMax = this.p4.z;
-  }
 }
 
-function setup() {
 
-}
-
-function draw() {
-
+// FUNCTION: draw Region mesh to screen
+function drawMesh( R ) {
+  // draw left and top sides for all Cells
+  for( var m = 0 ; m < M ; m++ ) {
+    for( var n = 0 ; n < N ; n++ ) {
+      var x1 = R.cells[m][n].p0.x * sFactor;
+      var y1 = R.cells[m][n].p0.y * sFactor;
+      var z1 = R.cells[m][n].p0.z * sFactor;
+      var x2 = R.cells[m][n].p1.x * sFactor;
+      var y2 = R.cells[m][n].p1.y * sFactor;
+      var z2 = R.cells[m][n].p1.z * sFactor;
+      beginShape();
+      vertex( x1 , y1 , z1 );
+      vertex( x2 , y2 , z2 );
+      endShape();
+      //line( x1 , y1 , z1 , x2 , y2 , z2 );
+      x1 = R.cells[m][n].p0.x * sFactor;
+      y1 = R.cells[m][n].p0.y * sFactor;
+      z1 = R.cells[m][n].p0.z * sFactor;
+      x2 = R.cells[m][n].p3.x * sFactor;
+      y2 = R.cells[m][n].p3.y * sFactor;
+      z2 = R.cells[m][n].p3.z * sFactor;
+      beginShape();
+      vertex( x1 , y1 , z1 );
+      vertex( x2 , y2 , z2 );
+      endShape();
+      //line( x1 , y1 , z1 , x2 , y2 , z2 );
+    }
+  }
+  // draw bottom side for cells (0,N-1) to (M-1,N-1)
+  for( var m = 0 ; m < M ; m++ ) {
+    n = N-1;
+    var x1 = R.cells[m][n].p2.x * sFactor;
+    var y1 = R.cells[m][n].p2.y * sFactor;
+    var z1 = R.cells[m][n].p2.z * sFactor;
+    var x2 = R.cells[m][n].p3.x * sFactor;
+    var y2 = R.cells[m][n].p3.y * sFactor;
+    var z2 = R.cells[m][n].p3.z * sFactor;
+    beginShape();
+    vertex( x1 , y1 , z1 );
+    vertex( x2 , y2 , z2 );
+    endShape();
+    //line( x1 , y1 , z1 , x2 , y2 , z2 );
+  }
+  // draw right side for cells (M-1,0) to (M-1,N-1)
+  for( var n = 0 ; n < N ; n++ ) {
+    m = M-1;
+    var x1 = R.cells[m][n].p2.x * sFactor;
+    var y1 = R.cells[m][n].p2.y * sFactor;
+    var z1 = R.cells[m][n].p2.z * sFactor;
+    var x2 = R.cells[m][n].p1.x * sFactor;
+    var y2 = R.cells[m][n].p1.y * sFactor;
+    var z2 = R.cells[m][n].p1.z * sFactor;
+    beginShape();
+    vertex( x1 , y1 , z1 );
+    vertex( x2 , y2 , z2 );
+    endShape();
+    //line( x1 , y1 , z1 , x2 , y2 , z2 );
+  }
 }
